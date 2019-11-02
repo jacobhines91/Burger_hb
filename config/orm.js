7f5d39eaf9813ff79359a4e21df613ac72c35af2
Jacob Hines <jacob_hines@hotmail.com>
2019-11-01 21:52:02 -0500

    config/model/controller config01

---
+++ config/orm.js
@@ -0,0 +1,66 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+// Object for all our SQL statement functions.
+var orm = {
+  selectAll: function(tb, cb) {
+    var queryString = "SELECT * FROM ?? ";
+    connection.query(queryString, [tb], function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  insertOne: function(tb, cols, vals, cb) {
+    var queryString = "INSERT INTO " + tb;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += vals;
+    queryString += ") ";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  // An example of objColVals would be {name: panther, sleepy: true}
+
+
+  updateOne: function (tb, colToUpdate, col, val, valToUpdate, cb) {
+    var queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+    connection.query(queryString, [tb, colToUpdate, col, val, valToUpdate], function(err, result) {
+        if (err) throw err;
+
+        console.table(result);
+        cb(result);
+    })
+}
+
+};
+
+//   update: function(table, objColVals, condition, cb) {
+//     var queryString = "UPDATE " + table;
+
+//     queryString += " SET ";
+//     queryString += objToSql(objColVals);
+//     queryString += " WHERE ";
+//     queryString += condition;
+
+//     console.log(queryString);
+//     connection.query(queryString, function(err, result) {
+//       if (err) {
+//         throw err;
+//       }
+
+//       cb(result);
+//     });
+//   },
+
+//export
+module.exports = orm;
---
+++ models/burger.js
@@ -0,0 +1,24 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm");
+  // The variables cols and vals are arrays.
+var burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            callback(res);
+        });
+    },
+
+    insertOne: function(vals, cb) {
+        orm.insertOne("burgers", ["burger_name", "devoured"], vals, function(res) {
+          
+        })
+    },
+
+    updateOne: function(valToUpdate, cb) {
+        orm.updateOne("burgers", "devoured", true, "id", valToUpdate, function(res) {
+            
+        })
+    }
+}
+// Export the database functions for the controller
+module.exports = burger;
